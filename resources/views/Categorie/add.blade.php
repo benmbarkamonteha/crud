95a2b44b554398124f5b840844590a4b60d02bae
benmbarka monteha <benmbarkamonteha@gmail.com>
2024-03-29 06:06:31 +0100

    add layout

--- resources/views/Categorie/add.blade.php
+++ resources/views/Categorie/add.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
-    <title>Add Category</title>
-</head>
-<body>
+@extends('categorie.layout.app')
+@section('content')
   <div class="container px-5 py-5">
 <form method="POST" action="{{ route('categorie.create') }}">
   @csrf
@@ -24,5 +15,4 @@
   <button type="submit" class="btn btn-primary"><i class="bi bi-plus-circle-fill"></i> Add Category</button>
 </form>
 </div>
-</body>
-</html>
+@endsection
--- resources/views/Categorie/index.blade.php
+++ resources/views/Categorie/index.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
-
-    <title>index</title>
-</head>
-<body>
+@extends('categorie.layout.app')
+@section('content')
     <div class="container mt-5">
         <a href="{{ URL::route('categorie.create') }}" class="btn btn-success"><i class="bi bi-plus-circle"></i> Add category </a>
         <br><br>
@@ -42,6 +33,4 @@
                 @endforeach
             </tbody>
         </table>
-    </div>
-</body>
-</html>
+        @endsection
